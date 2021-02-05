7e8dd0463e944a701912c66159e75ccced6f844a
Farshid Roohi <farshid.roohi.a@gmail.com>
2021-02-05 23:30:17 +0330

    Add Enum Configuration and fix dynamic layout direction home

--- EventApp/Constants.swift
+++ EventApp/Constants.swift
@@ -9,31 +9,35 @@
 import SwiftUI
 
 enum Constants {
-  static let baseUrl = "https://tedxtehran.idearun.co/graphql/"
-  static let placeholderUrl = "https://tedxtehran.com/"
-  static let acknowledgmentsUrl = "https://trello.com/b/9Vp84PlW"
+    static let baseUrl = "https://tedxtehran.idearun.co/graphql/"
+    static let placeholderUrl = "https://tedxtehran.com/"
+    static let acknowledgmentsUrl = "https://trello.com/b/9Vp84PlW"
+    
+    static var direction:LayoutDirection = .leftToRight
+    static var token:String = "56fe1591-c5c1-4113-aba3-73fca1f5aacd"
+    static var locale:Locale =  Locale(identifier: "en") //Locale(identifier: "ar_QA")
 }
 
 enum Fonts {
-  static let shabnam = "Shabnam-FD"
-  static let shabnamBold = "Shabnam-Bold-FD"
+    static let shabnam = ""//"Shabnam-FD"
+    static let shabnamBold = ""//"Shabnam-Bold-FD"
 }
 
 enum Colors {
-  static let primaryRed = Color("primaryRed")
-  static let primaryLightRed = Color("primaryLightRed")
-  static let primaryBackground = Color("primaryBackground")
-  static let primaryBarBackground = Color("primaryBarBackground")
-  static let primaryLightGray = Color("primaryLightGray")
-  static let primaryDarkGray = Color("primaryDarkGray")
+    static let primaryRed = Color("primaryRed")
+    static let primaryLightRed = Color("primaryLightRed")
+    static let primaryBackground = Color("primaryBackground")
+    static let primaryBarBackground = Color("primaryBarBackground")
+    static let primaryLightGray = Color("primaryLightGray")
+    static let primaryDarkGray = Color("primaryDarkGray")
 }
 
 enum Images {
-  static let urlExtension = "https://tedxtehran.idearun.co/media/"
-  static let logo = "tedx-logo"
-  static let placeholder = "placeholder"
-  static let profilePlaceholder = "profile-placeholder"
-  static let example = "example-image"
-  static let newsIconExample = "zitel"
-  static let emptyList = "404"
+    static let urlExtension = "https://tedxtehran.idearun.co/media/"
+    static let logo = "tedx-logo"
+    static let placeholder = "placeholder"
+    static let profilePlaceholder = "profile-placeholder"
+    static let example = "example-image"
+    static let newsIconExample = "zitel"
+    static let emptyList = "404"
 }
--- EventApp/SceneDelegate.swift
+++ EventApp/SceneDelegate.swift
@@ -22,7 +22,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         
         let contentView = AppView()
-//            .environment(\.locale,.init(identifier: "ar_QA"))
+            .environment(\.locale,Constants.locale)
         
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
