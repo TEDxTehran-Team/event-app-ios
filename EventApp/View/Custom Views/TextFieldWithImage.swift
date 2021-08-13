d10d4977161fc2801218ce5f60702c4868bd5249
Alireza <mr.toghyiani@icloud.com>
2021-08-13 19:00:32 +0430

    make phone nummber view UI better

---
+++ EventApp/View/Custom Views/TextFieldWithImage.swift
@@ -0,0 +1,49 @@
+//
+//  TextFieldWithImage.swift
+//  Formaloo
+//
+//  Created by Tadeh Alexani on 9/27/20.
+//
+
+import SwiftUI
+
+struct TextFieldWithImage: View {
+    
+    @Binding var text: String
+    let placeholder: String
+    let imageName: String
+    var isFieldSecure: Bool = false
+    var keyboardType: UIKeyboardType = .default
+    var textFieldIdentifier: String = ""
+    var secureTextFieldIdentifier: String = ""
+    
+    var body: some View {
+        HStack {
+            if imageName != "" {
+                Image(uiImage: UIImage(systemName: imageName) ?? UIImage(named: imageName)!)
+                    .resizable()
+                    .renderingMode(.template)
+                    .scaledToFit()
+                    .frame(width: 20, height: 20)
+                    .foregroundColor(.gray)
+                
+            }
+            if isFieldSecure {
+                SecureField(LocalizedStringKey(placeholder), text: $text)
+                    .accessibility(identifier: secureTextFieldIdentifier)
+                
+            } else {
+                TextField(LocalizedStringKey(placeholder), text: $text)
+                    .accessibility(identifier: textFieldIdentifier)
+                    .keyboardType(keyboardType)
+            }
+        }
+        .padding()
+        .overlay(
+            RoundedRectangle(cornerRadius: 5)
+                .stroke(lineWidth: 1)
+                .foregroundColor(.gray)
+        )
+    }
+}
+
--- EventApp/View/Login/PhoneNumberView.swift
+++ EventApp/View/Login/PhoneNumberView.swift
@@ -13,76 +13,51 @@ struct PhoneNumberView: View {
     @ObservedObject var viewModel = PhoneNumberViewModel()
     @Binding var loginToken: String
     
-    
     var body: some View {
-        VStack(alignment: .center) {
-            Spacer()
-            Image("tedx-logo")
-                .resizable()
-                .frame(height: 67)
-                .padding(20)
-            
-            Text(LocalizedStringKey("welcome_title_networking"))
-                .customFont(name: Configuration.shabnamBold, size: 36, weight: .bold)
-                .foregroundColor(Colors.darkTextColor)
-                .padding([.leading, .trailing], 10)
-                .padding(.bottom, 15)
-
-            
-            Text(LocalizedStringKey("networking_message"))
-                .customFont(name: Configuration.shabnam, size: 16, weight: .bold)
-                .foregroundColor(Colors.darkTextColor)
-                .padding([.leading, .trailing], 10)
-                .padding(.bottom, 40)
-                .lineLimit(4)
-
-            CustomTextField
-            
-            Spacer()
-            
-            chatButton
-                .padding(.bottom, 40)
-                .onTapGesture(count: 1, perform: {
-                    viewModel.setup() { token in
-                        self.loginToken = token
-                    }
-                })
+        GeometryReader { geo in
+            VStack(alignment: .leading, spacing: 20) {
+                Spacer()
+                Image("tedx-logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: geo.size.width*13/20)
+                
+                Text("welcome_title_networking".localized())
+                    .font(.headline)
+                    .bold()
+                
+                
+                Text("networking_message".localized())
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+                
+                TextFieldWithImage(text: $viewModel.phoneNumber, placeholder: "Phone Number", imageName: "iphone")
+                    .padding(.top, 20)
+                
+                Spacer()
+                
+                Text("Get Code".localized())
+                    .customFont(name: Configuration.shabnam, style: .headline, weight: .regular)
+                    .frame(width: geo.size.width - 40, height: 62, alignment: .center)
+                    .foregroundColor(Color.white)
+                    .background(Colors.primaryRed)
+                    .cornerRadius(5)
+                    .padding(.bottom, 40)
+                    .onTapGesture(count: 1, perform: {
+                        viewModel.setup() { token in
+                            self.loginToken = token
+                        }
+                    })
+            }
+            .padding()
         }
     }
 }
 
-extension PhoneNumberView {
-    private var chatButton: some View {
-        Text(LocalizedStringKey("Get Code"))
-            .customFont(name: Configuration.shabnam, style: .headline, weight: .regular)
-            .frame(width: 290, height: 62, alignment: .center)
-            .foregroundColor(Color.white)
-            .background(Colors.primaryRed)
-            .cornerRadius(5)
-    }
-    
-    var CustomTextField: some View {
-        HStack {
-            Image(systemName: "iphone")
-                .padding([.leading, .trailing], 5)
-            TextField(LocalizedStringKey("Phone Number").stringKey, text: $viewModel.phoneNumber)
-                .padding([.trailing], 5)
-                .textContentType(.telephoneNumber)
-                .keyboardType(.phonePad)
+struct PhoneNumberView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            PhoneNumberView(loginToken: .constant(""))
         }
-        .frame(height: 55)
-        .border(Color(.lightGray), width: 3)
-        .background(Color.clear)
-        .cornerRadius(6)
-        .padding(15)
     }
 }
-
-//struct PhoneNumberView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        @ObservedObject var settings = UserSettings()
-//        Group {
-//            PhoneNumberView(token: $settings.loginToken)
-//        }
-//    }
-//}
